a6cd6b034f43eadd046f8e8562b588dd95dfd985
Grace <wangarigrace198@gmail.com>
2024-10-22 13:37:10 +0300

    git

--- Frontend_Angular/src/app/services/http-service.service.ts
+++ Frontend_Angular/src/app/services/http-service.service.ts
@@ -30,7 +30,7 @@ export class HttpServiceService {
   //  serverUrl: string = 'http://192.168.88.241:8080/v1/api/';
   //serverUrl: string = 'http://192.168.89.134:8080/v1/api/';
 
-  serverUrl: string = 'http://172.16.9.46:8080/v1/api/';
+  serverUrl: string = 'http://172.16.11.17:8080/v1/api/';
 
   //serverUrl: string = environment.API_BASE_URL;
   //  serverUrl: string = 'http://192.168.2.21:8080';
